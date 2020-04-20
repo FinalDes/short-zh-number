e2d53fa9d563aac9431b941138f0629fab56136a
Alex Alonso <alex4551@gmail.com>
2020-04-20 16:48:27 +0800

    use eslint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 const unitList = " 萬億兆京垓秭穰溝澗正載".split("");
 const unitList1 = " 万亿兆京垓秭穰沟涧正载".split("");
-export function shortNumber(value: number|string, simplified: boolean = false): string {
-    let result: string = "";
+export function shortNumber(value: number|string, simplified = false): string {
+    let result = "";
     let editValue = +value;
     const list = simplified === true ? unitList1 : unitList;
     for (const unit of list) {
