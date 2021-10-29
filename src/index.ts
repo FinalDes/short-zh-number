7b3634cff7a03e32fe8427d19b35fe491d4323f1
Alex Alonso <alex4551@gmail.com>
2021-10-29 17:37:49 +0800

    change tslint to eslint and add prettier, add vscode devcontainer

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-export function shortNumber(value: number|string, simplified = false): string {
-    const unitList = " 萬億兆京垓秭穰溝澗正載".split("");
-    const unitList1 = " 万亿兆京垓秭穰沟涧正载".split("");
-    let editValue = +value;
-    const list = simplified === true ? unitList1 : unitList;
-    for (const unit of list) {
-        if (editValue >= 10000) {
-            editValue /= 10000.0;
-        } else if (!isNaN(editValue) && value !== null) {
-            return editValue.toLocaleString("en") + unit.trim();
-        }
+export function shortNumber(
+  value: number | string,
+  simplified = false
+): string {
+  const unitList = ' 萬億兆京垓秭穰溝澗正載'.split('');
+  const unitList1 = ' 万亿兆京垓秭穰沟涧正载'.split('');
+  let editValue = +value;
+  const list = simplified === true ? unitList1 : unitList;
+  for (const unit of list) {
+    if (editValue >= 10000) {
+      editValue /= 10000.0;
+    } else if (!isNaN(editValue) && value !== null) {
+      return editValue.toLocaleString('en') + unit.trim();
     }
-    return "";
+  }
+  return '';
 }
